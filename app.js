3f59abde1be5514af453c96232a85c42c32eab62
MatheusGarciia <garciiamatheus98@gmail.com>
2024-04-22 18:00:13 -0300

    Add buttons to on/off and selector of speed

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 let circles = document.querySelectorAll('#circles')
 let index = 0
+let powerButton = document.querySelector('#powerButton')
+let interval = null
+let inputTimer = document.getElementById('intervalTimer')
+let intervalTime = 0
+let submitButton = document.querySelector('#submitButton')
+
+function start(){
+
+    if (powerButton.className == 'off'){
+        intervalTime = inputTimer.value
+        intervalTime = intervalTime * 1000
+        powerButton.className = 'on'
+        interval = setInterval(printCircle, intervalTime)
+    } else {
+        powerButton.className = 'off'
+        clearInterval(interval)
+    }
+}
 
 function printCircle(){
     const circle = circles[index]
-
-    console.log(circle)
     circle.classList.add('active')
 
     setTimeout(() => {
         circle.classList.remove('active')
-    }, 1750)
+    }, 1000)
     
     if (index === circles.length - 1){
         index = 0
@@ -18,5 +34,4 @@ function printCircle(){
     }
 }
 
-setInterval(printCircle, 2000)
 
